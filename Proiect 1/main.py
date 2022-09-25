733b4529e88523f5277663c7972d4116c474cbb2
Ovidiu A <113139646+oviyce@users.noreply.github.com>
2022-09-25 10:38:41 +0300

    Tema 2 de la Cursul 3

--- Proiect 1/main.py
+++ Proiect 1/main.py
@@ -1,26 +1,68 @@
-# Tema 1. Creați un script Python care îndeplinește următoarele funcții:
+# Tema 2
 
-### 1.1 Declară o listă care conține elementele 7, 8, 9, 2, 3, 1, 4, 10, 5, 6 (în această ordine);
-lista_initiala = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+# Exercitiul_1: Să se scrie o funcție care primește un număr nedefinit de parametrii și
+#               să se calculeze suma parametrilor care reprezintă numere întregi sau reale.
+# >> your_function(1, 5, -3, ‘abc’, [12, 56, ‘cad’]) va returna 3 (1 + 5 -3)
+# >> your_function() va returna 0
+# >> your_function(2, 4, ‘abc’, param_1=2) va returna 6 (2 + 4)
 
-### 1.2 Afișează lista inițială ordonată ascendent (lista inițială trebuie păstrată în aceeași formă);
-lista_asc = lista_initiala.copy()
-lista_asc.sort()
-print(lista_asc)
+# Exercitiul_2: Să se scrie o funcție recursivă care primește ca parametru un număr întreg și returnează:
+# >> suma tuturor numerelor de la [0, n]
+# >> suma numerelor pare de la [0, n]
+# >> suma numerelor impare de la [0. n]
 
-### 1.3 Afișează lista inițială ordonată descendent (lista inițială trebuie păstrată în aceeași formă);
-lista_desc = lista_asc.copy()
-lista_desc.reverse()
-print(lista_desc)
+# Exercitiul_3: Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta
+#               este un număr întreg, altfel returnează valoarea 0.
 
-### 1.4 Afișează o listă ce conține numerele pare din lista ordonată (folosind slice);
-lista_pare = lista_asc[1::2]
-print(lista_pare)
+#-------------------------------------------------------------------------
+print('Tema 2 (nu se pune deoarece am facut-o dupa ce s-a rezolvat la curs)')
 
-### 1.5 Afișează o listă ce conține numerele impare din lista ordonată (folosind slice);
-lista_impare = lista_desc[1::2]
-print(lista_impare)
+print ('Exercitiul 1:')
+def my_function1 (*args, **kwargs):
+    my_sum1 = 0
+    for my_value1 in args:
+        if type(my_value1) == int or type(my_value1) == float:
+            my_sum1 += my_value1
+    return my_sum1
+print(my_function1(1, 5, -3, 'abc', [12, 56, 'cad']))
+print(my_function1())
+print(my_function1(2, 4, 'abc', param_1=2))
 
-### 1.6 Afisează o listă ce conține numerele ce sunt multipli ai numărului 3 (folosind slice).
-lista_multip3 = lista_asc[2::3]
-print(lista_multip3)
+#-------------------------------------------------------------------------
+print ('Exercitiul 2:')
+
+def my_function2(n):
+    if n == 0:
+        return 0, 0, 0
+
+    total, even, odd = my_function2(n-1)
+    total += n
+
+    if n % 2 == 0:
+        even += n
+    else:
+        odd += n
+
+    return total, even, odd
+
+sum_total, sum_even, sum_odd = my_function2(5)
+print('total = ', sum_total)
+print('even = ', sum_even)
+print('odd = ', sum_odd)
+
+#-------------------------------------------------------------------------
+print ('Exercitiul 3:')
+
+def my_function3():
+    x = input('>>')
+
+    try:
+        x = int(x)
+    except ValueError:
+        x = 0
+
+    return x
+
+print(my_function3())
+
+print('End of Homework')
